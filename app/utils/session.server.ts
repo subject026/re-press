a28e31c3c9e11955473c42844a5b6f30f739a6ac
subject026 <subject026@protonmail.com>
2023-01-21 11:02:18 +0000

    remove moved methods

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,32 +1,4 @@
-import { createCookieSessionStorage, redirect } from "@remix-run/node";
-import bcrypt from "bcryptjs";
-
-import { db } from "./db.server";
-
-type LoginForm = {
-  email: string;
-  password: string;
-};
-
-export async function register({ email, password }: LoginForm) {
-  const passwordHash = await bcrypt.hash(password, 10);
-  const user = await db.user.create({
-    data: { email, passwordHash },
-  });
-  return { id: user.id, email };
-}
-
-export async function login({ email, password }: LoginForm) {
-  const user = await db.user.findUnique({
-    where: { email },
-  });
-  if (!user) return null;
-
-  const isCorrectPassword = await bcrypt.compare(password, user.passwordHash);
-  if (!isCorrectPassword) return null;
-
-  return { id: user.id, email };
-}
+import { createCookieSessionStorage } from "@remix-run/node";
 
 const sessionSecret = process.env.SESSION_SECRET;
 if (!sessionSecret) {
@@ -48,62 +20,5 @@ const storage = createCookieSessionStorage({
   },
 });
 
-export async function getUser(request: Request) {
-  const userId = await getUserId(request);
-  if (typeof userId !== "string") {
-    return null;
-  }
-
-  try {
-    const user = await db.user.findUnique({
-      where: { id: userId },
-      select: { id: true, email: true },
-    });
-    return user;
-  } catch {
-    throw logout(request);
-  }
-}
-
-export async function logout(request: Request) {
-  const session = await getUserSession(request);
-  return redirect("/login", {
-    headers: {
-      "Set-Cookie": await storage.destroySession(session),
-    },
-  });
-}
-
-export async function createUserSession(userId: string, redirectTo: string) {
-  const session = await storage.getSession();
-  session.set("userId", userId);
-  return redirect(redirectTo, {
-    headers: {
-      "Set-Cookie": await storage.commitSession(session),
-    },
-  });
-}
-
-function getUserSession(request: Request) {
-  return storage.getSession(request.headers.get("Cookie"));
-}
-
-export async function getUserId(request: Request) {
-  const session = await getUserSession(request);
-  const userId = session.get("userId");
-  if (!userId || typeof userId !== "string") return null;
-  return userId;
-}
-
-export async function requireUserId(
-  request: Request,
-  redirectTo: string = new URL(request.url).pathname
-) {
-  const session = await getUserSession(request);
-  const userId = session.get("userId");
-  if (!userId || typeof userId !== "string") {
-    const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
-    throw redirect(`/login?${searchParams}`);
-  }
-  return userId;
-}
+// you can also export the methods individually for your own usage
+export const { getSession, commitSession, destroySession } = storage;
