867783800bcdb27c721a6fadc50a34a3b1be5d01
subject026 <subject026@protonmail.com>
2023-02-22 08:19:12 +0000

    fixed tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,43 +5,24 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("displays login form if not logged in", async ({ page }) => {
-  await expect(page.getByText("Bingleboogle")).toBeTruthy();
+  expect(await page.getByText("Login").count()).toBeTruthy();
 });
 
 test("can login with email and password", async ({ page }) => {
   // not showing dashboard
-  expect(await page.getByText("Admin Index").count()).toBeFalsy();
+  await expect(page.locator("body")).toContainText(/Login/);
 
   await page.getByTestId("email-input").type("steve@example.com");
   await page.getByTestId("password-input").type("password");
 
   await page.getByTestId("login-button").click();
 
-  await expect(page.getByText("Admin Index")).toBeTruthy();
+  await expect(page.locator("body")).toContainText(/steve@example.com/);
+  await expect(page.locator("body")).not.toContainText(/Login/);
 
-  const logoutButton = await page.getByTestId("logout-button");
+  await page.getByTestId("logout-button").click();
 
-  expect(logoutButton.count()).toBeTruthy();
-
-  await logoutButton.click();
-
-  expect(await page.getByText("Admin Index").count()).toBeFalsy();
-});
-
-// test.skip("see error message if login fails", async ({ page }) => {
-//   // TESTING: write test here
-// });
-
-test.skip("can logout", async ({ page }) => {
-  // await expect(page.getByText("Admin Indexyyy").isVisible()).toBeFalsy();
-  expect(await page.getByText("other text").count()).toBeFalsy();
-  expect(await page.getByText("Admin Index").count()).toBeTruthy();
-
-  const logoutButton = await page.getByTestId("logout-button");
-
-  expect(logoutButton).toBeTruthy();
-
-  // await page.getByTestId("logout-button").click();
-
-  // await expect(page.getByText("logoutnoooo")).toBeTruthy();
+  // back to login page
+  await expect(page.locator("body")).toContainText(/Login/);
+  await expect(page.locator("body")).not.toContainText(/steve@example.com/);
 });
